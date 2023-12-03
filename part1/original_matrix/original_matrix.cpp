e1050fd7bd03fb17d7ae3a106dd76581dac28b15
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-12-03 13:52:25 -0800

    Update original_matrix.cpp

--- part1/original_matrix/original_matrix.cpp
+++ part1/original_matrix/original_matrix.cpp
@@ -2,19 +2,29 @@
 #include "ap_fixed.h"
 #include "hls_math.h"
 
-void pl_vecadd(float a[1024], float b[1024], float c[1024])
+void partTwoPartA(int A[16][16], int B[16][16], int AB[16][16])
 {
-  #pragma HLS INTERFACE m_axi port=a offset=slave bundle=data0
-  #pragma HLS INTERFACE s_axilite register port=a bundle=ctrl
-  #pragma HLS INTERFACE m_axi port=b offset=slave bundle=data1
-  #pragma HLS INTERFACE s_axilite register port=b bundle=ctrl
-  #pragma HLS INTERFACE m_axi port=c offset=slave bundle=data2
-  #pragma HLS INTERFACE s_axilite register port=c bundle=ctrl
-  #pragma HLS INTERFACE s_axilite register port=return bundle=ctrl
-  for (int i = 0; i < 1024; i += 1)
-  {
-    #pragma HLS pipeline
-    c[i] = a[i] + b[i];
-  }
 
-}
+	int M = 16;
+	int N = 16;
+	int P = 16;
+	#pragma HLS INTERFACE m_axi port=A offset=slave bundle=data0
+	#pragma HLS INTERFACE s_axilite register port=A bundle=ctrl
+	#pragma HLS INTERFACE m_axi port=B offset=slave bundle=data1
+	#pragma HLS INTERFACE s_axilite register port=B bundle=ctrl
+	#pragma HLS INTERFACE m_axi port=AB offset=slave bundle=data2
+	#pragma HLS INTERFACE s_axilite register port=AB bundle=ctrl
+	#pragma HLS INTERFACE s_axilite register port=return bundle=ctrl
+	for(int i=0; i<N; ++i)
+	{
+		for(int j=0; j<P; ++j)
+		{
+			int ABij = 0;
+			for(int k=0; k < M; ++k)
+			{
+				ABij += A[i][k] * B[k][j];
+			}
+			AB[i][j] = ABij;
+		}
+	}
+ }
