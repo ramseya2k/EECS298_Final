9b884556b53c69047fc0abef618fdcbfe9bf05d8
jangs33 <152765357+jangs33@users.noreply.github.com>
2023-12-07 15:18:24 -0800

    Update layer3_fc6_origianl.cpp

--- part2/final/layer3_fc6_origianl.cpp
+++ part2/final/layer3_fc6_origianl.cpp
@@ -1,36 +1,12 @@
-#include "ap_int.h"
-#include "ap_fixed.h"
-#include "hls_math.h"
+#include <cmath>
+
+// Forward declarations
+void Convolution3(float input[16][10][10], float weights[16][6][5][5], float bias[16], float output[16][10][10]);
+void FC6(float input[16][10][10], float weights[10][120], float bias[10], float output[10]);
 
 void pl_lenet5_fpga(float image[32][32],
                     float conv3_weights[16][6][5][5], float conv3_bias[16], float conv3_output[16][10][10],
-                    float fc6_weights[10][120], float fc6_bias[10], float fc6_output[10])
-{
-    // Define AXI4 Master interfaces for input and output arrays
-    #pragma HLS INTERFACE m_axi port=image offset=slave bundle=data0
-    #pragma HLS INTERFACE s_axilite register port=image bundle=ctrl
-
-    #pragma HLS INTERFACE m_axi port=conv3_weights offset=slave bundle=data1
-    #pragma HLS INTERFACE s_axilite register port=conv3_weights bundle=ctrl
-
-    #pragma HLS INTERFACE m_axi port=conv3_bias offset=slave bundle=data2
-    #pragma HLS INTERFACE s_axilite register port=conv3_bias bundle=ctrl
-
-    #pragma HLS INTERFACE m_axi port=conv3_output offset=slave bundle=data3
-    #pragma HLS INTERFACE s_axilite register port=conv3_output bundle=ctrl
-
-    #pragma HLS INTERFACE m_axi port=fc6_weights offset=slave bundle=data4
-    #pragma HLS INTERFACE s_axilite register port=fc6_weights bundle=ctrl
-
-    #pragma HLS INTERFACE m_axi port=fc6_bias offset=slave bundle=data5
-    #pragma HLS INTERFACE s_axilite register port=fc6_bias bundle=ctrl
-
-    #pragma HLS INTERFACE m_axi port=fc6_output offset=slave bundle=data6
-    #pragma HLS INTERFACE s_axilite register port=fc6_output bundle=ctrl
-
-    // Define a control interface for the function
-    #pragma HLS INTERFACE s_axilite register port=return bundle=ctrl
-
+                    float fc6_weights[10][120], float fc6_bias[10], float fc6_output[10]) {
     // Convolution Layer 3
     Convolution3(image, conv3_weights, conv3_bias, conv3_output);
 
@@ -39,14 +15,10 @@ void pl_lenet5_fpga(float image[32][32],
 }
 
 // Convolution Layer 3 operation
-void Convolution3(float input[16][10][10], float weights[16][6][5][5], float bias[16], float output[16][10][10])
-{
+void Convolution3(float input[16][10][10], float weights[16][6][5][5], float bias[16], float output[16][10][10]) {
     for (int co = 0; co < 16; co++) {
         for (int h = 0; h < 10; h++) {
             for (int w = 0; w < 10; w++) {
-                // Placeholder for convolution logic
-                // You need to add your convolution logic here
-                // The following is a simple example; replace it with your implementation
                 float conv_sum = bias[co];
                 for (int ci = 0; ci < 6; ci++) {
                     for (int i = 0; i < 5; i++) {
@@ -55,15 +27,14 @@ void Convolution3(float input[16][10][10], float weights[16][6][5][5], float bia
                         }
                     }
                 }
-                output[co][h][w] = hls::relu(conv_sum); // Apply ReLU activation
+                output[co][h][w] = fmaxf(0.0, conv_sum); // Apply ReLU activation
             }
         }
     }
 }
 
 // Fully Connected Layer 6 operation
-void FC6(float input[16][10][10], float weights[10][120], float bias[10], float output[10])
-{
+void FC6(float input[16][10][10], float weights[10][120], float bias[10], float output[10]) {
     float fc6_input[120];
 
     // Flatten the input tensor
@@ -77,6 +48,6 @@ void FC6(float input[16][10][10], float weights[10][120], float bias[10], float 
         for (int ci = 0; ci < 120; ci++) {
             fc_sum += weights[co][ci] * fc6_input[ci];
         }
-        output[co] = hls::relu(fc_sum); // Apply ReLU activation
+        output[co] = fmaxf(0.0, fc_sum); // Apply ReLU activation
     }
 }
