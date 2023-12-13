5ea83c73ca23765ff27a67e1350d19a4a4f979cf
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-12-12 23:08:19 -0800

    Update fc6_optimized_1.cpp
    Changed the slave ports to the correct mappings.

--- part2/final/fc6_optimized_1.cpp
+++ part2/final/fc6_optimized_1.cpp
@@ -7,11 +7,11 @@ void fc6(const float input[120][1][1], const float weights[10][120][1][1],
          const float bias[10], float output[10]) 
 {
   #pragma HLS INTERFACE m_axi port=input offset=slave bundle=data0
-  #pragma HLS INTERFACE s_axilite register port=a bundle=ctrl
+  #pragma HLS INTERFACE s_axilite register port=input bundle=ctrl
   #pragma HLS INTERFACE m_axi port=weights offset=slave bundle=data1
-  #pragma HLS INTERFACE s_axilite register port=b bundle=ctrl
+  #pragma HLS INTERFACE s_axilite register port=weights bundle=ctrl
   #pragma HLS INTERFACE m_axi port=bias offset=slave bundle=data2
-  #pragma HLS INTERFACE s_axilite register port=c bundle=ctrl
+  #pragma HLS INTERFACE s_axilite register port=bias bundle=ctrl
   #pragma HLS INTERFACE m_axi port=output offset=slave bundle=data3
   #pragma HLS INTERFACE s_axilite register port=output bundle=ctrl
   #pragma HLS INTERFACE s_axilite register port=return bundle=ctrl
