300fb14d23ff489ff0aea3fa492c1854c6e8335d
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-12-13 17:02:25 -0800

    Update convo3.cpp

--- part2/final/convo/convo3.cpp
+++ part2/final/convo/convo3.cpp
@@ -17,18 +17,23 @@ void convolution3(float input[6][14][14], float weights[16][6][5][5],
 	
 	#pragma HLS PIPELINE II=1
 	
-	for(int co = 0; co < 16; co++)
+	for(int co = 0; co < 16; co++) {
         #pragma HLS PIPELINE II=1
-		for(int h = 0; h < 10; h++)
+        for(int h = 0; h < 10; h++) {
             for(int w = 0; w < 10; w++) {
-                    float sum = 0;
-					#pragma HLS UNROLL factor=5
-                    for(int i = h, m = 0; i < (h+5); i++, m++) {
-                        #pragma HLS UNROLL factor=5
-						for(int j = w, n = 0; j < (w+5); j++, n++)
-                            for (int ci = 0; ci < 6; ci++)
-                                sum += weights[co][ci][m][n] * input[ci][i][j];
+                float sum = 0;
+                #pragma HLS UNROLL factor=5
+                for(int i = h, m = 0; i < (h + 5); i++, m++) {
+                    #pragma HLS UNROLL factor=5
+                    for(int j = w, n = 0; j < (w + 5); j++, n++) {
+                        for (int ci = 0; ci < 6; ci++) {
+                            sum += weights[co][ci][m][n] * input[ci][i][j];
+                        }
                     }
-                    output[co][h][w] = sum + bias[co];
+                }
+                output[co][h][w] = sum + bias[co];
             }
+        }
+    }
+}
 }
