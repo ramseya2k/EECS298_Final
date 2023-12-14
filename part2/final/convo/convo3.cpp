f0317412d87816780fd4c8e63333a9ebf8c1bf03
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-12-13 16:50:31 -0800

    Update convo3.cpp

--- part2/final/convo/convo3.cpp
+++ part2/final/convo/convo3.cpp
@@ -8,10 +8,12 @@ void convolution3(float input[6][14][14], float weights[16][6][5][5],
     for(int co = 0; co < 16; co++)
         for(int h = 0; h < 10; h++)
             for(int w = 0; w < 10; w++) 
-			{
+			
+	    {
                     float sum = 0;
                     for(int i = h, m = 0; i < (h+5); i++, m++) 
-					{
+					
+		    {
                         for(int j = w, n = 0; j < (w+5); j++, n++)
                             for (int ci = 0; ci < 6; ci++)
                                 sum += weights[co][ci][m][n] * input[ci][i][j];
